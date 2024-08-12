2a0fcab95095485e1ac8357ee124f4c6360a102f
Contemporani Analtica <analitica@contemporani.com>
2024-08-12 22:35:45 +0000

    Fixes to reporte_compras

--- models/contemporani/marts/final_reporte_compras.sql
+++ models/contemporani/marts/final_reporte_compras.sql
@@ -2,16 +2,7 @@ with
 
 compras as (
     select
-        cidproducto,
-        ccodigoproducto,
-        cnombreproducto,
-        proveedor,
-        tipo,
-        cidautoincsql,
-        cpreciocompra,
-        cidmoneda,
-        fecha_compra,
-        monto_compra_pesos
+        * except(fecha_cambio, cimporte)
 
     from {{ ref('int_contemporani__compras_producto') }}
     order by fecha_compra desc

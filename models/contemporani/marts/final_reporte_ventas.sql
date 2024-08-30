f1ed309338567ff7392f974f3fbea4adf2a370e5
Contemporani Analtica <analitica@contemporani.com>
2024-08-30 15:20:25 +0000

    Fix a inventarios

--- models/contemporani/marts/final_reporte_ventas.sql
+++ models/contemporani/marts/final_reporte_ventas.sql
@@ -6,7 +6,7 @@ final as (
     from {{ ref('int_contemporani__ventas') }}
     where 
         ccodigoalmacen != '2' --  No deberia de haber productos en el almacen 2, son errores de contpaq
-        and ccodigoproducto IN ('PTOTAL', 'FLETE') -- No son productos verdaderos, son productos que ayudan para la operacion
+        and ccodigoproducto not IN ('PTOTAL', 'FLETE') -- No son productos verdaderos, son productos que ayudan para la operacion
 
 
 )
